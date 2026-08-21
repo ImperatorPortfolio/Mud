739c8ae5eaf9e9d8dcf8840bb7352cbecf95996d
Imperator <imperatormovil@gmail.com>
2026-08-21 19:12:16 +0200

    Remove redundant planet saver declaration

--- src/area_builder_commands.c
+++ src/area_builder_commands.c
@@ -14,7 +14,6 @@
 
 DECLARE_DO_FUN( legacy_do_aset );
 void write_area_list( void );
-void save_planet( PLANET_DATA *planet );
 
 static bool area_filename_component_valid( const char *component )
 {
